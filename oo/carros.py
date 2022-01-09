5f158b1ec18aca95a932d49d6bd224ce3e281804
adnirJunior <adabjunior@hotmail.com>
2022-01-09 19:38:37 -0300

    Criada a implementacao carro

--- oo/carros.py
+++ oo/carros.py
@@ -82,13 +82,13 @@ O   L
   >>> carro.calcular_direcao()
   'Norte'
   >>> carro.girar_a_direita()
-  >>> carro.calcular_a_direcao()
+  >>> carro.calcular_direcao()
   'Leste'
   >>> carro.girar_a_esquerda()
-  >>> carro.calcular_a_direcao()
+  >>> carro.calcular_direcao()
   'Norte'
   >>> carro.girar_a_esquerda()
-  >>> carro.calcular_a_direcao()
+  >>> carro.calcular_direcao()
   'Oeste'
 """
 NORTE = 'Norte'
@@ -120,8 +120,28 @@ class Direcao:
         elif self.valor == LESTE:
             self.valor = NORTE
 
+class Carro:
+    def __init__(self, direcao, motor):
+        self.direcao = direcao
+        self.motor = motor
 
+    def calcular_velocidade(self):
+        return self.motor.velocidade
 
+    def acelerar(self):
+        self.motor.acelerar()
+
+    def frear(self):
+        self.motor.frear()
+
+    def calcular_direcao(self):
+        return self.direcao.valor
+
+    def girar_a_direita(self):
+        self.direcao.girar_a_direita()
+
+    def girar_a_esquerda(self):
+        self.direcao.girar_a_esquerda()
 
 class Motor:
 
